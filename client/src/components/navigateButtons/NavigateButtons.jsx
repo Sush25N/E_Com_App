0786253c892d29ccfe1b8675e7bc4c15869cfa8c
Sush25N <sushgabhane123@gmail.com>
2023-12-22 17:16:24 +0530

    added increment and decrement button in the cart section

--- client/src/components/navigateButtons/NavigateButtons.jsx
+++ client/src/components/navigateButtons/NavigateButtons.jsx
@@ -1,35 +1,3 @@
-// import React from "react";
-// import { Button } from "@material-tailwind/react";
-
-// const NavigateButtons = () => {
-//   const buttons = [
-//     "Hoodies",
-//     "Dresses",
-//     "Suits",
-//     "Shoes",
-//     "T-Shirts",
-//     "Jeans",
-//     "Jackets",
-//     "Bags",
-//   ];
-
-//   return (
-//     <div>
-//       <div className="flex items-center justify-center py-8">
-//         {buttons.map((button, index) => {
-//           return (
-//             <div key={index} className="mr-4">
-//               <Button>{button}</Button>
-//             </div>
-//           );
-//         })}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default NavigateButtons;
-
 import React from "react";
 import { Button } from "@material-tailwind/react";
 import clothes from "../../assets/images/clothes.jpg";
--- client/src/features/slices/cartSlice.jsx
+++ client/src/features/slices/cartSlice.jsx
@@ -71,131 +71,50 @@ export const cartSlice = createSlice({
         return err;
       }
     },
+
+    incrementQuantity(state, action) {
+      const { id, size, color } = action.payload;
+      const product = state.cart.find(
+        (item) => item.id === id && item.size === size && item.color === color
+      );
+
+      if (product) {
+        product.amount += 1;
+        product.totalPrice += product.price;
+        state.totalAmount += 1;
+        state.totalPrice += product.price;
+      }
+    },
+
+    decrementQuantity(state, action) {
+      const { id, size, color } = action.payload;
+      const product = state.cart.find(
+        (item) => item.id === id && item.size === size && item.color === color
+      );
+
+      if (product) {
+        if (product.amount === 1) {
+          state.cart = state.cart.filter(
+            (item) =>
+              !(item.id === id && item.size === size && item.color === color)
+          );
+          state.totalAmount -= 1;
+          state.totalPrice -= product.price;
+        } else {
+          product.amount -= 1;
+          product.totalPrice -= product.price;
+          state.totalAmount -= 1;
+          state.totalPrice -= product.price;
+        }
+      }
+    },
   },
 });
 
-export const { addToCart, removeFromCart } = cartSlice.actions;
+export const {
+  addToCart,
+  removeFromCart,
+  incrementQuantity,
+  decrementQuantity,
+} = cartSlice.actions;
 export default cartSlice.reducer;
-
-// cartSlice.jsx
-
-// import { createSlice } from "@reduxjs/toolkit";
-
-// export const cartSlice = createSlice({
-//   name: "cart",
-//   initialState: {
-//     cart: [],
-//     amount: 0,
-//     totalAmount: 0,
-//     totalPrice: 0,
-//   },
-//   reducers: {
-//     addToCart(state, action) {
-//       const productId = action.payload;
-//       try {
-//         const exist = state.cart.find(
-//           (product) =>
-//             product.id === productId.id &&
-//             product.size === productId.size &&
-//             product.color === productId.color
-//         );
-//         if (exist) {
-//           exist.amount++;
-//           exist.totalPrice += productId.price;
-//           state.totalAmount++;
-//           state.totalPrice += productId.price;
-//         } else {
-//           state.cart.push({
-//             id: productId.id,
-//             price: productId.price,
-//             size: productId.size,
-//             amount: 1,
-//             img: productId.img,
-//             totalPrice: productId.price,
-//             name: productId.name,
-//             text: productId.text,
-//             color: productId.color,
-//           });
-//           state.totalAmount++;
-//           state.totalPrice += productId.price;
-//         }
-//       } catch (err) {
-//         return err;
-//       }
-//     },
-//     removeFromCart(state, action) {
-//       const productId = action.payload;
-
-//       try {
-//         const exist = state.cart.find(
-//           (product) =>
-//             product.id === productId.id &&
-//             product.size === productId.size &&
-//             product.color === productId.color
-//         );
-//         if (exist.amount === 1) {
-//           state.cart = state.cart.filter(
-//             (product) =>
-//               product.id !== productId.id ||
-//               product.size !== productId.size ||
-//               product.color !== productId.color
-//           );
-//           state.totalAmount--;
-//           state.totalPrice -= productId.price;
-//         } else {
-//           exist.amount--;
-//           exist.totalPrice -= productId.price;
-//           state.totalAmount--;
-//           state.totalPrice -= productId.price;
-//         }
-//       } catch (err) {
-//         return err;
-//       }
-//     },
-
-//     incrementQuantity(state, action) {
-//       const { id, size, color } = action.payload;
-//       const product = state.cart.find(
-//         (item) => item.id === id && item.size === size && item.color === color
-//       );
-
-//       if (product) {
-//         product.amount += 1;
-//         product.totalPrice += product.price;
-//         state.totalAmount += 1;
-//         state.totalPrice += product.price;
-//       }
-//     },
-
-//     decrementQuantity(state, action) {
-//       const { id, size, color } = action.payload;
-//       const product = state.cart.find(
-//         (item) => item.id === id && item.size === size && item.color === color
-//       );
-
-//       if (product) {
-//         if (product.amount === 1) {
-//           state.cart = state.cart.filter(
-//             (item) =>
-//               !(item.id === id && item.size === size && item.color === color)
-//           );
-//           state.totalAmount -= 1;
-//           state.totalPrice -= product.price;
-//         } else {
-//           product.amount -= 1;
-//           product.totalPrice -= product.price;
-//           state.totalAmount -= 1;
-//           state.totalPrice -= product.price;
-//         }
-//       }
-//     },
-//   },
-// });
-
-// export const {
-//   addToCart,
-//   removeFromCart,
-//   incrementQuantity,
-//   decrementQuantity,
-// } = cartSlice.actions;
-// export default cartSlice.reducer;
