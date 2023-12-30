4219bf8af6b2cdb08e62a662d6b459c0e23d1cbe
Sush25N <sushgabhane123@gmail.com>
2023-12-30 23:56:18 +0530

    pay now button with predefined user values

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,3 +1,96 @@
+// import React, { useState } from "react";
+// import {
+//   Card,
+//   CardHeader,
+//   CardBody,
+//   CardFooter,
+//   Typography,
+//   Input,
+//   Checkbox,
+//   Button,
+// } from "@material-tailwind/react";
+// import { login } from "../../features/slices/authSlice";
+// import { useDispatch } from "react-redux";
+// import "./login.css";
+
+// const Login = () => {
+//   const initialState = {
+//     name: "",
+//     password: "",
+//     image: "",
+//   };
+
+//   const [values, setValues] = useState(initialState);
+
+//   const onChange = (e) => {
+//     const { name, value } = e.target;
+//     setValues({ ...values, [name]: value });
+//   };
+
+//   const dispatch = useDispatch();
+
+//   return (
+//     <div className="grid grid-cols-1 items-center justify-items-center h-screen relative">
+//       <Card className="w-96 bg-gray-300 rounded-md shadow-md relative z-10">
+//         <CardHeader
+//           variant="gradient"
+//           color="gray"
+//           className="mb-4 grid h-28 place-items-center rounded-t-md relative z-20"
+//         >
+//           <Typography variant="h3" color="white">
+//             Sign In
+//           </Typography>
+//         </CardHeader>
+//         <CardBody className="flex flex-col gap-4 p-6 relative z-20">
+//           <Input
+//             label="Name"
+//             size="lg"
+//             type="text"
+//             name="name"
+//             value={values.name}
+//             onChange={onChange}
+//           />
+//           <Input
+//             label="Password"
+//             size="lg"
+//             type="password"
+//             name="password"
+//             value={values.password}
+//             onChange={onChange}
+//           />
+//           <Input
+//             label="Image URL address"
+//             size="lg"
+//             type="text"
+//             name="image"
+//             value={values.image}
+//             onChange={onChange}
+//           />
+//           <div className="-ml-2.5">
+//             <Checkbox label="Remember Me" />
+//           </div>
+//         </CardBody>
+//         <CardFooter className="pt-0">
+//           <Button
+//             variant="gradient"
+//             fullWidth
+//             onClick={() => dispatch(login(values))}
+//           >
+//             Sign In
+//           </Button>
+
+//         </CardFooter>
+//       </Card>
+//       <div className="absolute inset-0 bg-gradient-to-r from-blue-700 to-purple-800 opacity-40 rounded-md z-0"></div>
+//       <div className="absolute inset-0 z-0">
+//         <div className="curve"></div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Login;
+
 import React, { useState } from "react";
 import {
   Card,
@@ -9,25 +102,72 @@ import {
   Checkbox,
   Button,
 } from "@material-tailwind/react";
-import { login } from "../../features/slices/authSlice";
-import { useDispatch } from "react-redux";
 import "./login.css";
+import { useDispatch } from "react-redux";
+import { login, signup } from "../../features/slices/authSlice";
 
 const Login = () => {
   const initialState = {
     name: "",
     password: "",
+    confirmPassword: "",
+    phoneNumber: "",
     image: "",
   };
 
   const [values, setValues] = useState(initialState);
+  const [isSignUp, setIsSignUp] = useState(false);
+  const dispatch = useDispatch();
 
   const onChange = (e) => {
     const { name, value } = e.target;
     setValues({ ...values, [name]: value });
   };
 
-  const dispatch = useDispatch();
+  const handleSignIn = () => {
+    const userId = {
+      name: values.name,
+      password: values.password,
+    };
+
+    // Assuming you have a list of registered users in your state or props
+    const registeredUsers = [
+      { name: "Sushrut", password: "Sush@123" },
+      { name: "user2", password: "password2" },
+      // Add more registered users as needed
+    ];
+
+    // Check if the user is registered. If not, toggle to Sign Up view
+    const isUserRegistered = registeredUsers.some(
+      (user) =>
+        user.name.toLowerCase() === userId.name.toLowerCase() &&
+        user.password === userId.password
+    );
+
+    if (!isUserRegistered) {
+      setIsSignUp(true);
+    } else {
+      dispatch(login(userId));
+    }
+  };
+
+  const handleSignUp = () => {
+    const newUser = {
+      name: values.name,
+      password: values.password,
+      confirmPassword: values.confirmPassword,
+      phoneNumber: values.phoneNumber,
+    };
+
+    dispatch(signup(newUser));
+
+    // Optionally, you can reset the form fields or perform any other actions
+    setValues(initialState);
+  };
+
+  const toggleView = () => {
+    setIsSignUp((prevIsSignUp) => !prevIsSignUp);
+  };
 
   return (
     <div className="grid grid-cols-1 items-center justify-items-center h-screen relative">
@@ -38,7 +178,7 @@ const Login = () => {
           className="mb-4 grid h-28 place-items-center rounded-t-md relative z-20"
         >
           <Typography variant="h3" color="white">
-            Sign In
+            {isSignUp ? "Sign Up" : "Sign In"}
           </Typography>
         </CardHeader>
         <CardBody className="flex flex-col gap-4 p-6 relative z-20">
@@ -58,14 +198,26 @@ const Login = () => {
             value={values.password}
             onChange={onChange}
           />
-          <Input
-            label="Image URL address"
-            size="lg"
-            type="text"
-            name="image"
-            value={values.image}
-            onChange={onChange}
-          />
+          {isSignUp && (
+            <React.Fragment>
+              <Input
+                label="Confirm Password"
+                size="lg"
+                type="password"
+                name="confirmPassword"
+                value={values.confirmPassword}
+                onChange={onChange}
+              />
+              <Input
+                label="Phone Number"
+                size="lg"
+                type="text"
+                name="phoneNumber"
+                value={values.phoneNumber}
+                onChange={onChange}
+              />
+            </React.Fragment>
+          )}
           <div className="-ml-2.5">
             <Checkbox label="Remember Me" />
           </div>
@@ -74,12 +226,21 @@ const Login = () => {
           <Button
             variant="gradient"
             fullWidth
-            onClick={() => dispatch(login(values))}
+            onClick={() => (isSignUp ? handleSignUp() : handleSignIn())}
           >
-            Sign In
+            {isSignUp ? "Sign Up" : "Sign In"}
           </Button>
+          <div className="text-center mt-2">
+            <Typography variant="body2" color="gray">
+              {isSignUp ? "Already Registered? " : "Not Registered? "}
+              <Button variant="text" color="gradient" onClick={toggleView}>
+                {isSignUp ? "Sign In" : "Sign Up"}
+              </Button>
+            </Typography>
+          </div>
         </CardFooter>
       </Card>
+
       <div className="absolute inset-0 bg-gradient-to-r from-blue-700 to-purple-800 opacity-40 rounded-md z-0"></div>
       <div className="absolute inset-0 z-0">
         <div className="curve"></div>
--- client/src/features/slices/authSlice.jsx
+++ client/src/features/slices/authSlice.jsx
@@ -1,3 +1,48 @@
+// import { createSlice } from "@reduxjs/toolkit";
+
+// export const authSlice = createSlice({
+//   name: "auth",
+//   initialState: {
+//     user: JSON.parse(sessionStorage.getItem("authUser")) || {
+//       name: "",
+//       password: "",
+//       image: "",
+//       authUser: false,
+//     },
+//   },
+//   reducers: {
+//     login(state, action) {
+//       const userId = action.payload;
+//       const userValidation = /^[A-Za-z]{4,10}$/i.test(userId.name);
+//       const passwordValidation =
+//         /^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{4,10}$/i.test(
+//           userId.password
+//         );
+//       state.user = userId;
+
+//       if (!userValidation || !passwordValidation) {
+//         state.user.authUser = false;
+//       } else {
+//         state.user.authUser = true;
+//         const saveState = JSON.stringify(userId);
+//         sessionStorage.setItem("authUser", saveState);
+//       }
+//     },
+//     logout(state) {
+//       state.user = {
+//         name: "",
+//         password: "",
+//         image: "",
+//         authUser: false,
+//       };
+//       sessionStorage.clear();
+//     },
+//   },
+// });
+
+// export const { login, logout } = authSlice.actions;
+// export default authSlice.reducer;
+
 import { createSlice } from "@reduxjs/toolkit";
 
 export const authSlice = createSlice({
@@ -6,7 +51,6 @@ export const authSlice = createSlice({
     user: JSON.parse(sessionStorage.getItem("authUser")) || {
       name: "",
       password: "",
-      image: "",
       authUser: false,
     },
   },
@@ -28,11 +72,39 @@ export const authSlice = createSlice({
         sessionStorage.setItem("authUser", saveState);
       }
     },
+    signup(state, action) {
+      const newUser = action.payload;
+      const userValidation = /^[A-Za-z]{4,10}$/i.test(newUser.name);
+      const passwordValidation =
+        /^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{4,10}$/i.test(
+          newUser.password
+        );
+
+      // Assuming you have a confirmPassword field in your form
+      const confirmPasswordValidation =
+        newUser.password === newUser.confirmPassword;
+
+      // Assuming you have a phoneNumber field in your form
+      const phoneNumberValidation = /^[0-9]{10}$/i.test(newUser.phoneNumber);
+
+      if (
+        !userValidation ||
+        !passwordValidation ||
+        !confirmPasswordValidation ||
+        !phoneNumberValidation
+      ) {
+        state.user.authUser = false;
+      } else {
+        state.user = newUser;
+        state.user.authUser = true;
+        const saveState = JSON.stringify(newUser);
+        sessionStorage.setItem("authUser", saveState);
+      }
+    },
     logout(state) {
       state.user = {
         name: "",
         password: "",
-        image: "",
         authUser: false,
       };
       sessionStorage.clear();
@@ -40,5 +112,5 @@ export const authSlice = createSlice({
   },
 });
 
-export const { login, logout } = authSlice.actions;
+export const { login, signup, logout } = authSlice.actions;
 export default authSlice.reducer;
