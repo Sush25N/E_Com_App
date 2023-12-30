98a1ee910052452184ba51209de62b8282e9c4d9
Sush25N <sushgabhane123@gmail.com>
2023-12-31 00:54:02 +0530

    added animation to the payment modal

--- client/src/components/cart/Cart.jsx
+++ client/src/components/cart/Cart.jsx
@@ -172,8 +172,195 @@
 
 // export default Cart;
 
-import React from "react";
-import { Fragment, useState } from "react";
+// import React from "react";
+// import { Fragment, useState } from "react";
+// import {
+//   Button,
+//   Dialog,
+//   DialogBody,
+//   DialogHeader,
+//   DialogFooter,
+//   Tooltip,
+// } from "@material-tailwind/react";
+// import { useSelector, useDispatch } from "react-redux";
+// import {
+//   removeFromCart,
+//   incrementQuantity,
+//   decrementQuantity,
+// } from "../../features/slices/cartSlice";
+
+// const Cart = ({ openModal, setOpen }) => {
+//   const cart = useSelector((state) => state.cart.cart);
+//   const totalPrice = useSelector((state) => state.cart.totalPrice);
+//   const dispatch = useDispatch();
+
+//   const handleIncrement = (item) => {
+//     dispatch(
+//       incrementQuantity({ id: item.id, size: item.size, color: item.color })
+//     );
+//   };
+
+//   const handleDecrement = (item) => {
+//     dispatch(
+//       decrementQuantity({ id: item.id, size: item.size, color: item.color })
+//     );
+//   };
+
+//   const handlePayNow = () => {
+//     // Add your logic to handle payment
+//     console.log("Payment logic goes here");
+//   };
+
+//   return (
+//     <div>
+//       {cart.length > 0 ? (
+//         <Fragment>
+//           <Dialog
+//             className="border-0 outline-0"
+//             open={openModal}
+//             handler={() => setOpen(false)}
+//             animate={{
+//               mount: { scale: 1, y: 0 },
+//               unmount: { scale: 0.9, y: -100 },
+//             }}
+//           >
+//             <DialogHeader>CART</DialogHeader>
+//             <DialogBody
+//               divider
+//               className="flex flex-col justify-center items-start"
+//             >
+//               {cart.map((item, index) => {
+//                 return (
+//                   <div key={index}>
+//                     <div className="grid grid-cols-2 py-4">
+//                       {/* ... (existing code) */}
+//                       <div>
+//                         <img
+//                           className="h-[250px] rounded-md"
+//                           src={item.img}
+//                           alt={item.name}
+//                         />
+//                         <div className="flex flex-col items-start">
+//                           <h4 className="text-black text-base font-inter font-bold tracking-normal leading-none pt-2">
+//                             {item.name}
+//                           </h4>
+//                         </div>
+//                         <div className="max-w-xs">
+//                           <p className="text-black text-xs font-inter tracking-normal leading-none pt-2">
+//                             {item.text}
+//                           </p>
+//                         </div>
+//                       </div>
+//                       <div>
+//                         <p className="text-black text-sm font-inter tracking-normal leading-none pt-2">
+//                           Selected size:{" "}
+//                           <span className="ml-2">{item.size}</span>
+//                         </p>
+//                         <p className="text-black text-sm font-inter tracking-normal leading-none pt-2">
+//                           Selected color:{" "}
+//                           <span
+//                             className="ml-2 rounded px-2"
+//                             style={{ backgroundColor: item.color }}
+//                           ></span>
+//                         </p>
+//                         <p className="text-black text-sm font-inter tracking-normal leading-none pt-2">
+//                           {/* Increment Button */}
+//                           <Button
+//                             size="sm"
+//                             color="green"
+//                             ripple={true}
+//                             onClick={() => handleIncrement(item)}
+//                           >
+//                             +
+//                           </Button>
+//                           <span className="ml-2">{item.amount}</span>
+//                           {/* Decrement Button */}
+//                           <Button
+//                             size="sm"
+//                             color="red"
+//                             ripple={true}
+//                             onClick={() => handleDecrement(item)}
+//                           >
+//                             -
+//                           </Button>
+//                         </p>
+//                         <p className="text-black text-sm font-inter tracking-normal leading-none pt-2">
+//                           Single Item Price:{" "}
+//                           <span className="ml-2">{item.price}$</span>
+//                         </p>
+//                         <p className="text-black text-sm font-inter tracking-normal leading-none pt-2">
+//                           Total Item Price:{" "}
+//                           <span className="ml-2">{item.totalPrice}$</span>
+//                         </p>
+//                         <div className="pt-4">
+//                           <Tooltip
+//                             content="Remove from the Cart"
+//                             placement="bottom"
+//                           >
+//                             <Button
+//                               onClick={() => dispatch(removeFromCart(item))}
+//                               size="sm"
+//                               color="red"
+//                               ripple={true}
+//                               variant="filled"
+//                             >
+//                               Remove
+//                             </Button>
+//                           </Tooltip>
+//                         </div>
+//                       </div>
+//                     </div>
+//                   </div>
+//                 );
+//               })}
+//             </DialogBody>
+//             <DialogFooter className="flex justify-start items-center">
+//               <p className="text-black text-base font-inter tracking-normal leading-none pt-2">
+//                 Total Price of All Products:{" "}
+//                 <span className="ml-2">{totalPrice}$</span>
+//               </p>
+//               <Button
+//                 size="regular"
+//                 color="gradient"
+//                 ripple={true}
+//                 onClick={() => handlePayNow()}
+//               >
+//                 Pay Now
+//               </Button>
+//             </DialogFooter>
+//           </Dialog>
+//         </Fragment>
+//       ) : (
+//         <Fragment>
+//           <Dialog
+//             className="border-0 outline-0"
+//             open={openModal}
+//             handler={() => setOpen(false)}
+//             animate={{
+//               mount: { scale: 1, y: 0 },
+//               unmount: { scale: 0.9, y: -100 },
+//             }}
+//           >
+//             <DialogHeader>CART</DialogHeader>
+//             <DialogBody divider>
+//               <h1 className="text-black text-3xl font-inter font-bold tracking-normal leading-none py-4">
+//                 Cart is empty
+//               </h1>
+//               <p className="text-black text-base font-inter tracking-normal leading-none">
+//                 Add some products
+//               </p>
+//             </DialogBody>
+//           </Dialog>
+//         </Fragment>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default Cart;
+
+// Import necessary dependencies and components
+import React, { Fragment, useState } from "react";
 import {
   Button,
   Dialog,
@@ -188,11 +375,13 @@ import {
   incrementQuantity,
   decrementQuantity,
 } from "../../features/slices/cartSlice";
+import PaymentConfirmationModal from "./PaymentConfirmationModal";
 
 const Cart = ({ openModal, setOpen }) => {
   const cart = useSelector((state) => state.cart.cart);
   const totalPrice = useSelector((state) => state.cart.totalPrice);
   const dispatch = useDispatch();
+  const [selectedProduct, setSelectedProduct] = useState(null);
 
   const handleIncrement = (item) => {
     dispatch(
@@ -206,9 +395,8 @@ const Cart = ({ openModal, setOpen }) => {
     );
   };
 
-  const handlePayNow = () => {
-    // Add your logic to handle payment
-    console.log("Payment logic goes here");
+  const handlePayNow = (item) => {
+    setSelectedProduct(item);
   };
 
   return (
@@ -229,106 +417,118 @@ const Cart = ({ openModal, setOpen }) => {
               divider
               className="flex flex-col justify-center items-start"
             >
-              {cart.map((item, index) => {
-                return (
-                  <div key={index}>
-                    <div className="grid grid-cols-2 py-4">
-                      {/* ... (existing code) */}
-                      <div>
-                        <img
-                          className="h-[250px] rounded-md"
-                          src={item.img}
-                          alt={item.name}
-                        />
-                        <div className="flex flex-col items-start">
-                          <h4 className="text-black text-base font-inter font-bold tracking-normal leading-none pt-2">
-                            {item.name}
-                          </h4>
-                        </div>
-                        <div className="max-w-xs">
-                          <p className="text-black text-xs font-inter tracking-normal leading-none pt-2">
-                            {item.text}
-                          </p>
-                        </div>
+              {cart.map((item, index) => (
+                <div key={index}>
+                  <div className="grid grid-cols-2 py-4">
+                    <div>
+                      <img
+                        className="h-[250px] rounded-md"
+                        src={item.img}
+                        alt={item.name}
+                      />
+                      <div className="flex flex-col items-start">
+                        <h4 className="text-black text-base font-inter font-bold tracking-normal leading-none pt-2">
+                          {item.name}
+                        </h4>
                       </div>
-                      <div>
-                        <p className="text-black text-sm font-inter tracking-normal leading-none pt-2">
-                          Selected size:{" "}
-                          <span className="ml-2">{item.size}</span>
+                      <div className="max-w-xs">
+                        <p className="text-black text-xs font-inter tracking-normal leading-none pt-2">
+                          {item.text}
                         </p>
-                        <p className="text-black text-sm font-inter tracking-normal leading-none pt-2">
-                          Selected color:{" "}
-                          <span
-                            className="ml-2 rounded px-2"
-                            style={{ backgroundColor: item.color }}
-                          ></span>
-                        </p>
-                        <p className="text-black text-sm font-inter tracking-normal leading-none pt-2">
-                          {/* Increment Button */}
+                      </div>
+                    </div>
+                    <div>
+                      <p className="text-black text-sm font-inter tracking-normal leading-none pt-2">
+                        Selected size: <span className="ml-2">{item.size}</span>
+                      </p>
+                      <p className="text-black text-sm font-inter tracking-normal leading-none pt-2">
+                        Selected color:{" "}
+                        <span
+                          className="ml-2 rounded px-2"
+                          style={{ backgroundColor: item.color }}
+                        ></span>
+                      </p>
+                      <p className="text-black text-sm font-inter tracking-normal leading-none pt-2">
+                        <Button
+                          size="sm"
+                          color="green"
+                          ripple={true}
+                          onClick={() => handleIncrement(item)}
+                        >
+                          +
+                        </Button>
+                        <span className="ml-2">{item.amount}</span>
+                        <Button
+                          size="sm"
+                          color="red"
+                          ripple={true}
+                          onClick={() => handleDecrement(item)}
+                        >
+                          -
+                        </Button>
+                      </p>
+                      <p className="text-black text-sm font-inter tracking-normal leading-none pt-2">
+                        Single Item Price:{" "}
+                        <span className="ml-2">{item.price}$</span>
+                      </p>
+                      <p className="text-black text-sm font-inter tracking-normal leading-none pt-2">
+                        Total Item Price:{" "}
+                        <span className="ml-2">{item.totalPrice}$</span>
+                      </p>
+                      <div className="pt-4">
+                        <Tooltip
+                          content="Remove from the Cart"
+                          placement="bottom"
+                        >
                           <Button
-                            size="sm"
-                            color="green"
-                            ripple={true}
-                            onClick={() => handleIncrement(item)}
-                          >
-                            +
-                          </Button>
-                          <span className="ml-2">{item.amount}</span>
-                          {/* Decrement Button */}
-                          <Button
+                            onClick={() => dispatch(removeFromCart(item))}
                             size="sm"
                             color="red"
                             ripple={true}
-                            onClick={() => handleDecrement(item)}
+                            variant="filled"
                           >
-                            -
+                            Remove
                           </Button>
-                        </p>
-                        <p className="text-black text-sm font-inter tracking-normal leading-none pt-2">
-                          Single Item Price:{" "}
-                          <span className="ml-2">{item.price}$</span>
-                        </p>
-                        <p className="text-black text-sm font-inter tracking-normal leading-none pt-2">
-                          Total Item Price:{" "}
-                          <span className="ml-2">{item.totalPrice}$</span>
-                        </p>
-                        <div className="pt-4">
-                          <Tooltip
-                            content="Remove from the Cart"
-                            placement="bottom"
-                          >
-                            <Button
-                              onClick={() => dispatch(removeFromCart(item))}
-                              size="sm"
-                              color="red"
-                              ripple={true}
-                              variant="filled"
-                            >
-                              Remove
-                            </Button>
-                          </Tooltip>
-                        </div>
+                        </Tooltip>
+                        <Button
+                          onClick={() => handlePayNow(item)}
+                          size="sm"
+                          color="blue"
+                          ripple={true}
+                        >
+                          Pay Now
+                        </Button>
                       </div>
                     </div>
                   </div>
-                );
-              })}
+                </div>
+              ))}
             </DialogBody>
             <DialogFooter className="flex justify-start items-center">
               <p className="text-black text-base font-inter tracking-normal leading-none pt-2">
                 Total Price of All Products:{" "}
                 <span className="ml-2">{totalPrice}$</span>
               </p>
-              <Button
-                size="regular"
-                color="gradient"
-                ripple={true}
-                onClick={() => handlePayNow()}
-              >
-                Pay Now
-              </Button>
             </DialogFooter>
           </Dialog>
+          {selectedProduct && (
+            <Dialog open={true}>
+              <DialogHeader>PAYMENT CONFIRMATION</DialogHeader>
+              <DialogBody divider>
+                <PaymentConfirmationModal product={selectedProduct} />
+              </DialogBody>
+              <DialogFooter>
+                <Button
+                  onClick={() => setSelectedProduct(null)}
+                  size="sm"
+                  color="blue"
+                  ripple={true}
+                >
+                  Close
+                </Button>
+              </DialogFooter>
+            </Dialog>
+          )}
         </Fragment>
       ) : (
         <Fragment>
---
+++ client/src/components/cart/PaymentConfirmationModal.jsx
@@ -0,0 +1,94 @@
+// import React from "react";
+
+// const PaymentConfirmationModal = ({ product }) => {
+//   return (
+//     <div className="flex flex-col items-center text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-96 p-6">
+//       <div className="relative overflow-hidden text-gray-700 bg-white bg-clip-border rounded-xl h-96 w-full mb-4">
+//         <img
+//           src={product.img}
+//           alt={product.name}
+//           className="object-cover w-full h-full"
+//         />
+//       </div>
+//       <div className="w-full">
+//         <div className="flex items-center justify-between mb-1">
+//           <p className="block font-sans text-base antialiased font-medium leading-relaxed text-blue-gray-900">
+//             {product.name}
+//           </p>
+//           <span className="ml-2 font-bold">Product Count: 1</span>
+//           <p className="block font-sans text-base antialiased font-medium leading-relaxed text-blue-gray-900">
+//             {product.price}$
+//           </p>
+//         </div>
+//         <div className="flex justify-between p-6 pt-4">
+//           <p className="font-bold">Total Amount: {product.totalPrice}$</p>
+//           <p className="font-bold">Total Count: {product.amount}</p>
+//         </div>
+//         <div>
+//           <div style={{ fontWeight: "bold" }}>Cheers : 🍻🍺</div>
+//           <div style={{ fontWeight: "bold" }}>
+//             Payment Done Successfully 🎉🎉✅✅🎊🎊
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default PaymentConfirmationModal;
+
+import React, { useState, useEffect } from "react";
+import Confetti from "react-confetti";
+
+const PaymentConfirmationModal = ({ product }) => {
+  const [confetti, setConfetti] = useState(false);
+
+  useEffect(() => {
+    // Trigger the confetti animation when the component mounts
+    setConfetti(true);
+
+    // Set a timeout to stop the confetti after a certain duration
+    const timeoutId = setTimeout(() => {
+      setConfetti(false);
+    }, 15000); // Stop confetti after 5 seconds (adjust as needed)
+
+    // Cleanup the timeout to prevent memory leaks
+    return () => clearTimeout(timeoutId);
+  }, []);
+
+  return (
+    <div className="flex flex-col items-center text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-96 p-6">
+      <div className="relative overflow-hidden text-gray-700 bg-white bg-clip-border rounded-xl h-96 w-full mb-4">
+        <img
+          src={product.img}
+          alt={product.name}
+          className="object-cover w-full h-full"
+        />
+      </div>
+      <div className="w-full">
+        <div className="flex items-center justify-between mb-1">
+          <p className="block font-sans text-base antialiased font-medium leading-relaxed text-blue-gray-900">
+            {product.name}
+          </p>
+          <span className="ml-2 font-bold">Product Count: 1</span>
+          <p className="block font-sans text-base antialiased font-medium leading-relaxed text-blue-gray-900">
+            {product.price}$
+          </p>
+        </div>
+        <div className="flex justify-between p-6 pt-4">
+          <p className="font-bold">Total Amount: {product.totalPrice}$</p>
+          <p className="font-bold">Total Count: {product.amount}</p>
+        </div>
+        <div>
+          <div style={{ fontWeight: "bold" }}>Cheers : 🍻🍺</div>
+          <div style={{ fontWeight: "bold" }}>
+            Payment Done Successfully 🎉🎉✅✅🎊🎊
+          </div>
+          {confetti && <Confetti />}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PaymentConfirmationModal;
